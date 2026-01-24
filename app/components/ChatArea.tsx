8ea3f05f9f5a91fcc5693f061be241cb9c5cb92c
Gabriel Chaves <gabriel.chaves.3720@outlook.com>
2026-01-24 17:38:08 -0300

    FEAT: readded timeout to scroll

--- app/components/ChatArea.tsx
+++ app/components/ChatArea.tsx
@@ -69,9 +69,11 @@ export default function ChatArea({ messages, isLoading, dict }: ChatAreaProps) {
   const chatContainerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    if (chatContainerRef.current) {
-      chatContainerRef.current.scrollTo({ top: chatContainerRef.current.scrollHeight, behavior: "smooth" });
-    }
+    setTimeout(() => {
+      if (chatContainerRef.current) {
+        chatContainerRef.current.scrollTo({ top: chatContainerRef.current.scrollHeight, behavior: "smooth" });
+      }
+    }, 250);
   }, [messages, isLoading]);
 
   return (
